7c937e9e90ee76c7a8e4939b2820457514825aab
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2025-02-15 16:51:12 -0300

    feat: adiciona solução do exercício cap01_1.2

---
+++ src/tauanbenevides/cap_01/Exercise01_02.java
@@ -0,0 +1,13 @@
+package tauanbenevides.cap_01;
+
+public class Exercise01_02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java five times");
+        System.out.println("Welcome to Java five times");
+        System.out.println("Welcome to Java five times");
+        System.out.println("Welcome to Java five times");
+        System.out.println("Welcome to Java five times");
+    }
+}
