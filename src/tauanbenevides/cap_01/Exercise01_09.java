02c4c11b2606ecf60fef5fe1fb18235c91ace627
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2025-02-15 16:59:53 -0300

    feat: adiciona solução do exercício cap01_1.9

---
+++ src/tauanbenevides/cap_01/Exercise01_09.java
@@ -0,0 +1,19 @@
+package tauanbenevides.cap_01;
+
+public class Exercise01_09 {
+
+    public static void main(String[] args) {
+
+        double width = 4.5;
+        double height = 7.9;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("The area of a rectangle with a width of 4.5 and a height"
+                + " of 7.9 is " + area);
+
+        System.out.println("Perimeter is " + perimeter);
+
+    }
+}
