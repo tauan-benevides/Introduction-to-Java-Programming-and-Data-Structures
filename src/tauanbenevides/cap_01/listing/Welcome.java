1084750f0b764ad512caffa8417307b83a238a2f
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2025-02-20 22:10:32 -0300

    feat: adiciona o programa da listagem 1.1 Welcome.java

---
+++ src/tauanbenevides/cap_01/listing/Welcome.java
@@ -0,0 +1,16 @@
+package tauanbenevides.cap_01.listing;
+
+/**
+ * Listing 1.1
+ * 
+ * Display message Welcome to Java! on the console
+ */
+
+public class Welcome {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java!");
+
+    }
+}
