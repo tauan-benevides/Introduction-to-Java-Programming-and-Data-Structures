b76560c0cbd945facb0ca9d6d1d3f9f46d91a804
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2025-02-20 22:14:07 -0300

    feat: adiciona o programa da listagem 1.4 ShowSyntaxErrors.java

---
+++ src/tauanbenevides/cap_01/listing/ShowSyntaxErrors.java
@@ -0,0 +1,22 @@
+package tauanbenevides.cap_01.listing;
+
+/**
+ * Listing 1.4
+ * 
+ * Display the syntax erro detected by the compile
+ */
+
+/*
+ * Erros de syntax ou de compilação são aqueles que ocorrem devido a digitação
+ * de uma palavra chave incorretamente, omição de alguma pontuação ou o
+ * esquecimento de abertura e fechamentos de chaves "{ }"
+ */
+
+public class ShowSyntaxErrors {
+
+    public static void main(String[] args) {
+
+        // System.out.println("Welcome to Java);
+
+    }
+}
