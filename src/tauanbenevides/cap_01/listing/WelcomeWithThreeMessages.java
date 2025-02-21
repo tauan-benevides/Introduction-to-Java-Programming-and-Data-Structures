2a472cdfb77b061407114afdf1346b65c225d34f
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2025-02-20 22:12:13 -0300

    feat: adiciona o programa da listagem 1.2 WelcomeWithThreeMessages.java

---
+++ src/tauanbenevides/cap_01/listing/WelcomeWithThreeMessages.java
@@ -0,0 +1,18 @@
+package tauanbenevides.cap_01.listing;
+
+/**
+ * Listing 1.2
+ * 
+ * Display three messages on the console
+ */
+
+public class WelcomeWithThreeMessages {
+
+    public static void main(String[] args) {
+
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
+
+    }
+}
