bebd8412890c7317d42e96563fcc79aff4b0b9c8
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2025-02-20 22:13:18 -0300

    feat: adiciona o programa da listagem 1.3 ComputeExpression.java

---
+++ src/tauanbenevides/cap_01/listing/ComputeExpression.java
@@ -0,0 +1,18 @@
+package tauanbenevides.cap_01.listing;
+
+/**
+ * Listing 1.3
+ * 
+ * Display the resulted mathematical computations the result on the console:
+ * 10.5 + (2 * 3) / 45 - 3.5
+ */
+
+public class ComputeExpression {
+
+    public static void main(String[] args) {
+
+        System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+
+    }
+}
