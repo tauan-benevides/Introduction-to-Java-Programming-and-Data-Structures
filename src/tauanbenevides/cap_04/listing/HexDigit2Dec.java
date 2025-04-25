31044bfc7f45528096965e249e0a09d3523867f8
Tauan Benevides <tg.dossantosbenevides@gmail.com>
2025-04-25 10:59:10 -0300

    feat: adiciona o programa da listagem 4.4 HexDigit2Dec.java

---
+++ src/tauanbenevides/cap_04/listing/HexDigit2Dec.java
@@ -0,0 +1,41 @@
+package tauanbenevides.cap_04.listing;
+
+import java.util.Scanner;
+
+/**
+ * Listing 4.4
+ * <p>
+ * program that prompts the user to enter a hex digit and display
+ * its corresponding decimal value
+ */
+
+public class HexDigit2Dec {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a hex digit: ");
+        String hexString = input.nextLine();
+
+        // Check if the hex string has exactly one character
+        if (hexString.length() != 1) {
+            System.out.println("You must enter exactly one character");
+            System.exit(1);
+        }
+
+        // Display decimal value for the hex digit
+        char ch = Character.toUpperCase(hexString.charAt(0));
+        if ('A' <= ch && ch <= 'F') {
+            int value = ch - 'A' + 10;
+            System.out.println("The decimal value for hex digit " +
+                    ch + " is " + value);
+        } else if (Character.isDigit(ch)) {
+            System.out.println("The decimal value for hex digit " +
+                    ch + " is " + ch);
+        } else {
+            System.out.println(ch + " is an invalid input");
+        }
+        input.close();
+    }
+}
